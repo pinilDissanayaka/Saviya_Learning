db00615cfcd21a7bbf1751f7108751ec9df761aa
= <pinildissanayaka@gmail.com>
2024-08-01 09:15:45 +0530

    Setup models

--- Backend/database.py
+++ Backend/database.py
@@ -1,3 +1,14 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+load_dotenv()
+
+DATABASE_URL=os.getenv('DATABASE_URL')
+
+engine=create_engine(url=DATABASE_URL)
+
+Base=declarative_base()
+
---
+++ Backend/schema.py
@@ -0,0 +1,12 @@
+from typing import Optional, List
+from enum import Enum
+from uuid import UUID, uuid4
+from pydantic import BaseModel, EmailStr
+
+
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4()
+    user_name: str
+    email:EmailStr
+    
