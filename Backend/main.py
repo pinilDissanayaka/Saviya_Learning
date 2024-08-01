2d60dff7d1da2fb266e98523fe41a99bbafb63ff
= <pinildissanayaka@gmail.com>
2024-08-01 16:35:27 +0530

    register api done

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from schema import User
 from models import UserModel
 from database import localSession
+from passlib.hash import pbkdf2_sha512
 
 
 app=FastAPI()
@@ -17,11 +18,18 @@ async def register(user:User):
     if existingUser:
         raise HTTPException(400, detail="Email already registered")
     else:
+        password=pbkdf2_sha512.hash(user['password'])
+        user.pop('password')
+        user['password']=password
         newUser=UserModel(**user)
         session.add(newUser)
         session.commit()
         session.refresh(newUser)
         
+        return {"detail":"user created successfully"}
+        
+        
+          
 @app.post("/add-resources/{user_id}")
 async def addResource(user_id: int):
     pass
