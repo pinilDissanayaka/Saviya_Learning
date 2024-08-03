2d4eb1f74451c42c1815a40ce60e842d7dafbec3
= <pinildissanayaka@gmail.com>
2024-08-03 18:12:37 +0530

    define chat template

--- saviya_be/utils/chain.py
+++ saviya_be/utils/chain.py
@@ -16,7 +16,17 @@ class Chain(object):
         
         
     def getChain():
-        pass
+        template = """Given the following context and a question, generate an answer based on this context only.
+                            In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
+                            If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+
+                            CONTEXT: {context}
+
+                            QUESTION: {question}"""
+                            
+        chatPromptTemplate=ChatPromptTemplate.from_template(template=template)
+                            
+
         
     
 
