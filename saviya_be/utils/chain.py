54024a5a89cee0650834cccfd9943f1c24bf1885
= <pinildissanayaka@gmail.com>
2024-08-03 18:19:55 +0530

    define chat template

--- saviya_be/utils/chain.py
+++ saviya_be/utils/chain.py
@@ -15,7 +15,7 @@ class Chain(object):
                           temperature=0.7)
         
         
-    def getChain():
+    def getChain(question:str):
         template = """Given the following context and a question, generate an answer based on this context only.
                             In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
                             If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
