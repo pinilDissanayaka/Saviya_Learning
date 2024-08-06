f50597b59d413435b931e99cc7f3213d30f9f6f2
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2024-08-06 23:54:42 +0530

    promt setup x

--- saviya_be/utils/chain.py
+++ saviya_be/utils/chain.py
@@ -1,38 +1,43 @@
 import os
 from dotenv import load_dotenv
 from langchain_groq.chat_models import ChatGroq
-from langchain.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+from langchain_core.prompts import ChatPromptTemplate,PromptTemplate,HumanMessagePromptTemplate,SystemMessagePromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-
 
 load_dotenv()
 
-os.environ['GROQ_API_KEY']=os.getenv('GROQ_API_KEY')
-os.environ['LANGCHAIN_API_KEY']=os.getenv('LANGCHAIN_API_KEY')
-os.environ['LANGCHAIN_PROJECT']=os.getenv('LANGCHAIN_PROJECT')
-os.environ['LANGCHAIN_TRACING_V2']=os.getenv('LANGCHAIN_TRACING_V2')
+os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
+os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
 
 class Chain(object):
     def __init__(self) -> None:
-        self.llm=ChatGroq(model="llama3-8b-8192",
-                          temperature=0.7)
-        
-        
-    def getChain(question:str)->str:
+        self.llm = ChatGroq(model="llama3-8b-8192", temperature=0.7)
+    
+    def getChain(self, question: str):
         template = """Given the following context and a question, generate an answer based on this context only.
-                            In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
-                            If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+                      In the answer try to provide as much text as possible from the "response" section in the source document context without making much changes.
+                      If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
 
-                            CONTEXT: {context}
+                      CONTEXT: {context}
 
-                            QUESTION: {question}"""
+                      QUESTION: {question}"""
                             
-        chatPromptTemplate=ChatPromptTemplate.from_template(template=template)
-                            
-
+        chatPromptTemplate = ChatPromptTemplate.from_template(template=template)
+        output_parser = StrOutputParser()
+ 
+        chain = (
+            chatPromptTemplate
+            | self.llm
+            | output_parser
+        )
         
+        return chain.invoke({"context": question, "question": question})
     
 
+def main():
+    chain = Chain()
+    d = chain.getChain(question="What is the capital of Nigeria?")
+    print(d)
 
-
+if __name__ == "__main__":
+    main()
